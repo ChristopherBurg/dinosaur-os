062ee47a589047f92c3a3108ae7efa8cadd8bc7e
Christopher Burg <christopherburg@christopherburg.com>
2025-11-09 19:09:56 -0600

    All of the Git installed extensions should be removed now.

--- build_files/base/01-base-image-changes.sh
+++ build_files/base/01-base-image-changes.sh
@@ -19,6 +19,24 @@ dnf5 remove -y \
 # Remove the AppIndicator extension.
 rm -rf /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/
 
+# Remove Blue My Shell.
+rm -rf /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/
+
+# Remove Caffeine.
+rm -rf /usr/share/gnome-shell/extensions/caffeine@patapon.info/
+
+# Remove Dash to Dock.
+rm -rf /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
+
+# Remove GSConnect
+rm -rf /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/
+
+# Remove Logo Menu.
+rm -rf /usr/share/gnome-shell/extensions/logomenu@aryan_k/
+
+# Remove Search Light.
+rm -rf /usr/share/gnome-shell/extensions/search-light@icedman.github.com/
+
 # I don't like hyperlinks polluting my application menu. The discourse.desktop
 # and documentation.desktop files simply open links to websites. Therefore, I
 # set them to hidden by injecting Hidden=true into them.
